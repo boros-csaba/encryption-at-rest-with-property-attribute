f47347dda9d4d6b965f03dc6e9a42a13a43bdaff
Csaba Boros <boros.csaba94@gmail.com>
2023-06-24 23:58:36 +0200

    Setup test project

---
+++ EncryptionAtRest.Tests/EncryptionTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace EncryptionAtRest.Tests
+{
+    public class EncryptionTests
+    {
+        [Fact]
+        public void DummyTest()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
