511b9225ba438975980eed477487d704d63b31f4
Csaba Boros <boros.csaba94@gmail.com>
2023-06-25 14:06:03 +0200

    Add migrations

---
+++ EncryptionAtRest/Migrations/20230625113536_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EncryptionAtRest.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Apis",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Key = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Apis", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Apis");
+        }
+    }
+}
---
+++ EncryptionAtRest/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace EncryptionAtRest
+{
+    class Program : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            return new AppDbContext("test");
+        }
+    }
+}
