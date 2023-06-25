acf4cf24ecff7c72fea2c9e1b9e11724920f31e8
Csaba Boros <boros.csaba94@gmail.com>
2023-06-25 16:42:59 +0200

    Test

--- EncryptionAtRest.Tests/EncryptionTests.cs
+++ EncryptionAtRest.Tests/EncryptionTests.cs
@@ -1,16 +1,40 @@
-﻿using Xunit;
+﻿using EncryptionAtRest.Database;
+using EncryptionAtRest.Database.Models;
+using FluentAssertions;
+using Xunit;
 
 namespace EncryptionAtRest.Tests
 {
-    public class EncryptionTests
+    public class EncryptionTests : IClassFixture<Fixture>
     {
+        private readonly AppDbContext _db;
+
+        public EncryptionTests(Fixture fixture)
+        {
+            _db = fixture.DbContext;
+        }
+
         [Fact]
         public void DummyTest()
         {
             // Arrange
+            var testApi = new Api
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test API",
+                Key = "Test API Key"
+            };
+
+
             // Act
+            _db.Apis.Add(testApi);
+            var testApiFromDb = _db.Apis.Find(testApi.Id);
+
+            
             // Assert
-            Assert.True(true);
+            testApiFromDb?.Id.Should().Be(testApi.Id);
+            testApiFromDb?.Name.Should().Be(testApi.Name);
+            testApiFromDb?.Key.Should().Be(testApi.Key);
         }
     }
 }
